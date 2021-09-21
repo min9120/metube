13e8cb48cd9d8c968c91e6c6573faec473adf043
gmin <gmin@yunsang-gyeongmin-MacBookAir.local>
2021-09-21 22:01:28 +0900

    create controller

---
+++ src/routes/userRouter.js
@@ -0,0 +1,8 @@
+import express from "express"
+import {edit, deleteUser} from "../controllers/userController"
+const userRouter = express.Router();
+
+userRouter.get("/edit", edit)
+userRouter.get("/delete", deleteUser)
+
+export default userRouter;

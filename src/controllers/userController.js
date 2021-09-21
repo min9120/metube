5772404055852b4d460d920bd7d2e2efe06201cf
min9120 <a9120a@gmail.com>
2021-09-21 22:49:52 +0900

    add routes and contollers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,6 @@
-export const join = (req, res)=> res.send("join page");
-export const edit = (req, res)=> res.send("edit user page");
-export const deleteUser = (req, res)=> res.send("delete user page");
+export const join = (req, res) => res.send("join page");
+export const edit = (req, res) => res.send("edit user page");
+export const deleteUser = (req, res) => res.send("delete user page");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
-export const trendingVideo = (req, res)=> res.send("Home page video");
-export const watch = (req, res)=> res.send("watch video");
-export const edit = (req, res)=> res.send("edit video");
+export const trendingVideo = (req, res) => res.send("Home page video");
+export const see = (req, res) => res.send("watch video");
+export const edit = (req, res) => res.send("edit video");
+export const search = (req, res) => res.send("search page");
+export const deleteVideo = (req, res) => res.send("delete video page");
+export const upload = (req, res) => res.send("upload video page");
